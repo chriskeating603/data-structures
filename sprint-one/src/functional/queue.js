2385d655e770be41d2c74924d73c5f1731bf9ba9
Hack Reactor Student <student@hackreactor.com>
2017-03-29 15:06:25 -0700

    Passed all functional queue tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,28 @@
 var Queue = function() {
   var someInstance = {};
-
+  
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var firstInLine = 1;
+  var lastInLine = 1;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[lastInLine] = value;
+    lastInLine++
   };
 
   someInstance.dequeue = function() {
+    if (firstInLine !== lastInLine) {
+      var deleteValue = storage[firstInLine];
+      delete storage[firstInLine];
+      firstInLine++;
+      return deleteValue;
+    }
   };
 
   someInstance.size = function() {
+    return lastInLine - firstInLine;
   };
 
   return someInstance;
