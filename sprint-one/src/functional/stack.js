1779625cf70a3c130f9e152bdaf5486f1d4822c7
Hack Reactor Student <student@hackreactor.com>
2017-03-29 15:07:16 -0700

    Passed all functional stack tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -7,10 +7,24 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // add someInstance to the storage at count
     count++;
+    storage[count] = value;
   };
 
   someInstance.pop = function() {
+    var remove = storage[count];
+    if(count > 0){
+      delete storage[count];
+      count--;
+      return remove;
+
+    }
+    // set var = storage at count - 1
+    // if count is greater than 0 
+      // delete storage at count
+      // return var
+
   };
 
   someInstance.size = function() {
