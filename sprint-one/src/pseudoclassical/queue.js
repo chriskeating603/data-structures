7a855f07567eeec0b4b321e3aa6cc9d761f31e05
Hack Reactor Student <student@hackreactor.com>
2017-03-30 10:37:03 -0700

    Passed all tests and pomander syntax for Pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.firstInLine = 1;
+  this.lastInLine = 1;
+  this.storage = {};
+};
+
+Queue.prototype.size = function() {
+  return this.lastInLine - this.firstInLine;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.lastInLine] = value;
+  this.lastInLine++;
+};
+
+Queue.prototype.dequeue = function() {
+  var deleted = this.storage[this.firstInLine];
+  if (this.lastInLine > this.firstInLine) {
+    delete this.storage[this.firstInLine];
+    this.firstInLine++;
+    return deleted;
+  }
 };
 
 
