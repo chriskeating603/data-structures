c04c86969d9345f0dbf1f1d9e59f54ee95b713d0
Hack Reactor Student <student@hackreactor.com>
2017-03-30 10:29:29 -0700

    Passed all tests and pomander syntax for functional shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instanceOf = {
+    firstInLine: 1,
+    lastInLine: 1,
+    storage: {}
+  };
+  _.extend(instanceOf, queueMethods);
+  return instanceOf;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function () {
+    return this.lastInLine - this.firstInLine;
+  },
+  enqueue: function (value) {
+    this.storage[this.lastInLine] = value;
+    this.lastInLine++;
+  },
+  dequeue: function () {
+    var deleted = this.storage[this.firstInLine];
+    if (this.lastInLine > this.firstInLine) {
+      delete this.storage[this.firstInLine];
+      this.firstInLine++;
+      return deleted;
+    }
+  }
+};
 
 
