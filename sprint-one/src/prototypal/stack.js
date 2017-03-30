1a48e3e444ce945d8ce9617b2f8b920f041553ac
Hack Reactor Student <student@hackreactor.com>
2017-03-30 10:35:30 -0700

    Passed all tests and pomander syntax for prototypal Stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instanceOf = Object.create(stackMethods);
+  instanceOf.count = 0;
+  instanceOf.storage = {};
+  return instanceOf;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function () {
+    return this.count;
+  },
+  push: function (value) {
+    this.count++;
+    this.storage[this.count] = value;
+  }, 
+  pop: function () {
+    var deleted = this.storage[this.count];
+    if (this.count > 0) {
+      delete this.storage[this.count];
+      this.count--;
+      return deleted;
+    }
+  }
+};
 
 
