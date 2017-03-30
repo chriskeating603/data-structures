ce89f0ab87eb0a047720feb4150a87c21eb42e2d
Hack Reactor Student <student@hackreactor.com>
2017-03-30 10:38:08 -0700

    Passed all tests and pomander syntax for pseudoclassical Stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
+};
+
+Stack.prototype.size = function() {
+  return this.count;
+};
+
+Stack.prototype.push = function(value) {
+  this.count++;
+  this.storage[this.count] = value;
+};
+
+Stack.prototype.pop = function() {
+  var deleted = this.storage[this.count];
+  if (this.count > 0) {
+    delete this.storage[this.count];
+    this.count--;
+    return deleted;
+  }
 };
 
 
