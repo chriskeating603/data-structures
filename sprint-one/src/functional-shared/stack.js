781eb79a12bf1d279051c74e102bd2cae20a0dde
Hack Reactor Student <student@hackreactor.com>
2017-03-30 10:32:17 -0700

    Passed all tests and pomander syntax for functional shared Stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instanceOf = {
+    storage: {},
+    count: 0
+  };
+  _.extend(instanceOf, stackMethods);
+  return instanceOf;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {
+    return this.count;
+  },
+  push: function (value) {
+    this.count++;
+    this.storage[this.count] = value;
+  },
+  pop: function () {
+    var deleted = this.storage[this.count];
+    if (this.count > 0) {
+      delete this.storage[this.count];
+      this.count--;
+      return deleted;
+    }
+  } 
+};
 
 
