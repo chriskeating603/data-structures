60ed7447f1e4608ac2402dd3e2f8520037af212b
Hack Reactor Student <student@hackreactor.com>
2017-03-31 22:06:34 -0700

    Passing all tests and pomander syntax for hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,14 +5,35 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[index] === undefined) {
+    this._storage[index] = [];
+  }
+
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      this._storage[index][i][1] = v; 
+      return;
+    }
+  }
+  this._storage[index].push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (k === this._storage[index][i][0]) {
+      return this._storage[index][i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (k === this._storage[index][i][0]) {
+      this._storage[index].splice(i, 1);
+    }
+  }
 };
 
 
