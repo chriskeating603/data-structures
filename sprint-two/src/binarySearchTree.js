cb52827e502d04619f87340f0651a516ade81d7e
Hack Reactor Student <student@hackreactor.com>
2017-03-31 22:03:13 -0700

    Conceptualized BinarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,24 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(binarySearchMethods);
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+};
+
+var binarySearchMethods = {
+  insert: function (value) {
+    // Check if tree.value !== undefined
+      // if tree.value is greater than our current value
+        // if tree.left.value is greater than our current value ....
+      // if no then if tree.right.value is greatter than our current value .....
+
+      //
+      
+
+    // 
+  },
+  contains: function (value) {},
+  depthFirstLog: function (cb) {}
 };
 
 
